1f1c4445ab0274eb711a00afee877c2bbc79b135
czajnus123 <patzer.daniel@gmail.com>
2018-11-22 22:46:40 +0100

    cc

--- ClimbClimb/Assets/Scripts/AIScript.cs
+++ ClimbClimb/Assets/Scripts/AIScript.cs
@@ -47,10 +47,6 @@ public class AIScript : MonoBehaviour {
 
     }
 
-   /* public void OnCollisionEnter2D(Collision2D collision)
-    {
-        change_side();
-    }*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag != "Player")
